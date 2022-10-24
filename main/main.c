770661ffbcd38204b5058508eb643e78cd1dfad8
FredrikCarlsson <fredrik.carlsson.92@gmail.com>
2022-10-24 17:16:53 +0200

    Added functionality to control GPIO

--- main/main.c
+++ main/main.c
@@ -5,18 +5,19 @@
 #include "IWifi.h"
 #include "IHTTP_Client.h"
 #include "IUDP_Client.h"
+#include "IGPIO.h"
 
 void app_main(void)
 {
 	printf("Starting Wifi scan!!!!\n");
 	IWifi_fast_scan();
-
+	IGPIO_read();
 	while(1)
 	{
 		sleep(3);
-		printf("Sending UDP packet!\n");
-		udp_client_task();
+		//printf("Sending UDP packet!\n");
+		//udp_client_task();
 		//IHttp_get();
-		printf("Done!\n");
+		//printf("Done!\n");
 	}
 }
